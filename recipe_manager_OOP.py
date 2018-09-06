cf540153057d31870a2602c4212ebe3407df9bbb
Jonah Harmon <jharmon3@tulane.edu>
2018-09-06 11:11:33 -0500

    Added a username system. Creates a folder with that username.

--- recipe_manager_OOP.py
+++ recipe_manager_OOP.py
@@ -20,11 +20,25 @@ INTENDED FEATURES:
 9) Grocery list generator based on a number of selected recipes.
 10) Recipe grabber. Take a url from the user, scan the webpage, and automatically load in and save 
     the recipe.
-11) Make it pretty.
+11) Cooking tracker. Show how many times in a week/month you cooked, give ability to sort or color by 
+    ingredient.
+12) Make it pretty.
 
 """
 
 import tkinter as tk
+from tkinter import ttk
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from matplotlib import style
+style.use('ggplot')
+import urllib
+import json
+import pandas as pd
+import numpy as np
+import os
 
 LARGE_FONT= ("Verdana", 12)
 
@@ -32,18 +46,23 @@ LARGE_FONT= ("Verdana", 12)
 class RecipeManager(tk.Tk):
     
     def __init__(self, *args, **kwargs):
+        
         tk.Tk.__init__(self, *args, **kwargs)
+        # tk.Tk.iconbitmap(self,default='clienticon.ico')
+        tk.Tk.wm_title(self, "Recipe Manager")
 
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
+
+        container.pack(side="top", fill="both", expand = True)
+
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
-
-        frame.grid(row=0, column=0, sticky="nsew")
+        for F in (StartPage, PageOne, PageTwo, PageThree):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
@@ -55,13 +74,108 @@ class RecipeManager(tk.Tk):
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self,parent)
 
-        label = tk.Label(self, text="This is the start page", font=LARGE_FONT)
+        label = ttk.Label(self, text="Enter Username:", font=LARGE_FONT)
+        label.pack(pady=20,padx=10)
+        entry = ttk.Entry(self)
+        entry.pack(ipadx = 30)
+        button = ttk.Button(self, text="Login",
+                            command=lambda:self.getUsernameReturning(entry))
+        button.pack()
+
+        label2 = ttk.Label(self,text='New User? Enter Username Below:',
+                      font=LARGE_FONT)
+        label2.pack(pady=80,padx=10)
+        entry2 = ttk.Entry(self)
+        entry2.pack(ipadx = 30)
+        button2 = ttk.Button(self, text="Login",
+                            command=lambda:self.getUsernameNew(entry2))
+        button2.pack()
+
+        button3 = ttk.Button(self,text='visit page 3',
+                             command=lambda:controller.show_frame(PageThree))
+        button3.pack()
+    
+    def getUsernameReturning(self, entry):
+        usernameReturning = entry.get()
+        # If directory does not exist, display 'username not found'
+        thisFileDir = os.getcwd() + '\\' + 'RecipeManager'
+        if(os.path.isdir(thisFileDir + '\\' + usernameReturning)):
+            print('It exists!')
+            # Do things with that folder, e.g. get recipes, store them, whatever
+        elif(not(os.path.isdir(thisFileDir + '\\' + usernameReturning))):
+            entry.delete(0, len(usernameReturning))
+            entry.insert(0, "That username does not exist.")
+    
+    def getUsernameNew(self, entry):
+        usernameNew = entry.get()
+        # If directory does not exist, display 'username already exists'
+        thisFileDir = os.getcwd() + '\\' + 'RecipeManager'
+        if(not(os.path.isdir(thisFileDir + '\\' + usernameNew))):
+            os.mkdir(thisFileDir + '\\' + usernameNew)
+        elif(os.path.isdir(thisFileDir + '\\' + usernameNew)):
+            entry.delete(0, len(usernameNew))
+            entry.insert(0, "That username is already taken.")
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page One!!!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-		
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
+
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+        
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 app = RecipeManager()
+app.geometry("720x480")
 app.mainloop()
 
 """
