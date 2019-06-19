bfe6618122e532a1e2da21e6b803496d26d1e873
Jonah Harmon <jsharmon@umich.edu>
2019-06-18 23:35:31 -0500

    Added tabs to QScrollArea to account for different monitor sizes; populated AddPage, currently no functionality.

--- RecipeManagerPyQt5_tabbed.py
+++ RecipeManagerPyQt5_tabbed.py
@@ -5,10 +5,15 @@ import recipeDatabaseInterface as rdi
 import os
 import sys
 
-# Subclass QWidget - just an empty container - to serve as the container for the login page
-class LoginPage(wid.QWidget):
+# Subclass QDialog - just an empty container - to serve as the container for the login page
+# Have the login run as a dialog before going to main app
+class LoginDialog(wid.QDialog): 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Create variable for DB path to associate with dialog instance later,
+        # pass to recipe manager upon creation
+        self.DBPath = ''
         
         # Set window title
         self.setWindowTitle('Login')
@@ -63,9 +68,10 @@ class LoginPage(wid.QWidget):
         recipePath = os.path.join(os.getcwd(), 'RecipeManager', userNameReturning)
 
         if(os.path.isdir(recipePath)):
-            # Repopulate QLineEdit to indicate user can now navigate to
-            # other pages in app after successful login
-            lineEdit.setText('Login successful! Use tabs to navigate to other pages...')
+            # Repopulate QLineEdit, close dialog, set path to pass to other page classes
+            self.DBPath = recipePath
+            lineEdit.setText('Login successful!')
+            self.accept()
 
         # If directory does not exist, display 'That username does not exist.'
         else:
@@ -92,9 +98,10 @@ class LoginPage(wid.QWidget):
             # Create new SQL database to hold recipes
             rdi.createNewDB(recipePath)
 
-            # Repopulate QLineEdit to indicate user can now navigate to
-            # other pages in app after successful login
-            lineEdit.setText('Account creation successful! Use tabs to navigate to other pages...')
+            # Repopulate QLineEdit, close dialog, set path to pass to other classes
+            self.DBPath = recipePath
+            lineEdit.setText('Account creation successful!')
+            self.accept()
 
         # If directory exists, display 'That username is already taken.'
         else:
@@ -102,10 +109,111 @@ class LoginPage(wid.QWidget):
 
 
 
+# Subclass QWidget to serve as empty container for home page
+class HomePage(wid.QWidget):
+    def __init__(self, DBPath, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Set path to DB
+        self.DBPath = DBPath
+
+
+
+# Subclass QWidget to serve as empty container for add page
+class AddPage(wid.QWidget):
+    def __init__(self, DBPath, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Set path to DB
+        self.DBPath = DBPath
+
+        # Set layout; this is a vertical box orientation
+        layout = wid.QVBoxLayout()
+
+        # Populate entries for AddPage, get text entry objects to get text later
+        textEntries = self.populateEntries(layout)
+
+        # Create add button, connect a signal (button press)
+        addButton = wid.QPushButton('Add Recipe')
+        addButton.pressed.connect(lambda: self.addCallback(textEntries))
+        layout.addWidget(addButton)
+
+        # Set layout
+        self.setLayout(layout)
+
+    def addCallback(self, textEntries):
+        # Get the text from each entry, read into list of strings
+        entryText = []
+        for entry in textEntries:
+            # For line edits, use .text() method
+            if type(entry) == type(wid.QLineEdit()):
+                entryText.append(entry.text())
+
+            if type(entry) == type(wid.QTextEdit()):
+                entryText.append(entry.toPlainText())
+
+        # Now, actually do some SQL stuff to add the recipe to the DB
+        ############################################################################################################################
+        # Need to implement recipe adding! will come from recipeDatabaseInterface.py!
+        ############################################################################################################################
+
+    def populateEntries(self, layout):
+        # Names of text entries to add
+        lineEntriesFirst = ['Recipe Name:', 'Recipe Summary:', "Today's Date (mm/dd/yyyy):", 'Type of cuisine:']
+        plainTextEdits = ['Ingredients (format - ingredient,amount,unit then enter):', 'Recipe Text:']
+        lineEntriesSecond = ['Prep Time:', 'Cook Time:', 'Calories:', 'Servings:', 'Tags:']
+
+        # Create empty list to fill with entry objects
+        entries = []
+
+        # Create all entries, append objects to list
+        for name in lineEntriesFirst:
+            labelEntryF = self.newLabelEntry(layout, name)
+            entries.append(labelEntryF)
+        for name in plainTextEdits:
+            textEdit = self.newPlainTextEdit(layout, name)
+            entries.append(textEdit)
+        for name in lineEntriesSecond:
+            labelEntryS = self.newLabelEntry(layout, name)
+            entries.append(labelEntryS)
+
+        return entries
+
+    def newLabelEntry(self, layout, name):
+        # Generate label and add to layout
+        label = wid.QLabel(name)
+        layout.addWidget(label)
+
+        # Generate associated text entry and add to layout
+        entry = wid.QLineEdit()
+        layout.addWidget(entry)
+
+        # Return entry to get text later
+        return entry
+
+    def newPlainTextEdit(self, layout, name):
+        # Generate label and add to layout
+        label = wid.QLabel(name)
+        layout.addWidget(label)
+
+        # Generate associated plain text edit and add to layout
+        edit = wid.QTextEdit()
+        layout.addWidget(edit)
+
+        return edit
+
+
+
 # Subclass QMainWindow to serve as empty container for z-stack of page widgets
 class RecipeManager(wid.QMainWindow):   
-    def __init__(self, *args, **kwargs):
+    def __init__(self, DBPath, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Set DB path, passed from dialog
+        self.DBPath = DBPath
+
+        # Set window title
+        self.setWindowTitle('Recipe Manager')
 
         # Generate tab widget to contain pages, navigate using tabs
         tabs = wid.QTabWidget()
@@ -114,22 +222,34 @@ class RecipeManager(wid.QMainWindow):
         tabs.setMovable(True)
 
         # Generate list of page names for labeling tabs
-        pageNames = ['Login', 'Home', 'Add', 'Random', 'Search', 'Import Recipe',
+        pageNames = ['Home', 'Add', 'Random', 'Search', 'Import Recipe',
                      'Meal Planning', 'Grocery List', 'Meal Tracking']
         
         # Generate list of page objects for assigning to tabs
-        pageObjects = [LoginPage(), LoginPage(), LoginPage(), LoginPage(), LoginPage(), LoginPage(),
-                       LoginPage(), LoginPage(), LoginPage()]
+        pageObjects = [HomePage(self.DBPath), AddPage(self.DBPath), HomePage(self.DBPath), HomePage(self.DBPath), 
+                       HomePage(self.DBPath), HomePage(self.DBPath), HomePage(self.DBPath), HomePage(self.DBPath)]
 
         for name, pageObj in zip(pageNames, pageObjects):
             tabs.addTab(pageObj, name)
 
-        self.setCentralWidget(tabs)
+        # Create scroll area in case window is resized, contain tabs within it
+        scrollArea = wid.QScrollArea()
+        scrollArea.setMinimumWidth(tabs.sizeHint().width())
+        scrollArea.setHorizontalScrollBarPolicy(core.Qt.ScrollBarAlwaysOff)
+        scrollArea.setWidget(tabs)
+        scrollArea.setWidgetResizable(True)
+
+        # Set the scroll area as the central widget
+        self.setCentralWidget(scrollArea)
 
 
 
 if __name__ == '__main__':
     app = wid.QApplication([])
-    window = RecipeManager()
-    window.show()
-    app.exec_()
+    login = LoginDialog()
+    
+    # If dialog has been accepted, close and run main app
+    if login.exec_() == wid.QDialog.Accepted:
+        window = RecipeManager(login.DBPath)
+        window.show()
+        app.exec_()
